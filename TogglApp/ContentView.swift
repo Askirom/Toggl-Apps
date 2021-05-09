f1ccbaf1e1f2ae0f2feff8a2bd273252f1c1ae5e
Varlokk <59657229+Varlokk@users.noreply.github.com>
2021-05-09 22:53:27 +0200

    Small stuff

--- TogglApp/ContentView.swift
+++ TogglApp/ContentView.swift
@@ -9,21 +9,8 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
-    var body: some View {
-            VStack {
-                Text("Turtle Rock")
-                    .font(.title)
-                Text("Joshua Tree National Park")
-            }
-        }
-    }
+    
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
